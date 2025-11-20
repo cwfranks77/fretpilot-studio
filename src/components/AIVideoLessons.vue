902b50323695f97397022c48692eca1b2c41d1ac
FretPilot Developer <dev@fretpilot.com>
2025-11-20 00:52:53 -0500

    Fix: Use width/height instead of right/bottom for fixed modals

--- src/components/AIVideoLessons.vue
+++ src/components/AIVideoLessons.vue
@@ -740,8 +740,8 @@ export default {
   position: fixed;
   top: 0;
   left: 0;
-  right: 0;
-  bottom: 0;
+  width: 100%;
+  height: 100%;
   background: rgba(0, 0, 0, 0.8);
   display: flex;
   align-items: center;
@@ -787,8 +787,8 @@ export default {
   position: fixed;
   top: 0;
   left: 0;
-  right: 0;
-  bottom: 0;
+  width: 100%;
+  height: 100%;
   background: rgba(0, 0, 0, 0.9);
   display: flex;
   align-items: center;
